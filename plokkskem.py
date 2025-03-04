ae887e64082a48d09e646bd6d89688cd83af52a2
Настя Радашева <eha20082@gmail.com>
2025-03-04 20:15:56 +0200

    new

--- plokkskem.py
+++ plokkskem.py
@@ -41,3 +41,42 @@ try:
         print(f"nado {skokmesta} podhodov")
 except:
     print("viga ")
+
+
+    #V4 2. Koostage programmi plokkskeem, et arvutada ainult negatiivsete P antud arvude summa.
+vastus=0
+P=int(input("Mitu korda kordame?"))
+while True:
+    arv=float(input("Sisesta arv: "))
+    if arv<0: vastus+=arv
+    P-=1
+    if P==0: break
+print("Summa on: ",vastus)
+
+vastus=0
+P=int(input("Mitu korda kordame?"))
+for i in range(P):
+    arv=float(input("Sisesta arv: "))
+    if arv<0: vastus+=arv
+print("Summa on: ",vastus)
+#V1 4. Koostage plokkskeem kotlette praadiva roboti jaoks.
+kokku=int(input("Kokku kotlete: "))
+panni_maht=int(input("Panni maht: "))
+aeg=1
+lahenemine=kokku//panni_maht
+jaak=kokku%panni_maht
+if jaak>0: lahenemine+=1
+print(f"Praeme. Tuleb {lahenemine} lahenemist.")
+for l in range(lahenemine):
+    if jaak>0 and l==lahenemine-1:
+        print(f"Panni peal on {jaak} kotlet.")
+    else:
+        print(f"Panni peal on {panni_maht} kotlet.")
+    print(f"{l+1}. lahenemine. Praeme esimene pool.")
+    sleep(aeg)
+    print("Ümberpöörame")
+    print(f"{l+1}. lahenemine. Praeme teine pool.")
+    sleep(aeg)
+    print(f"Valmis!")
+    
+print("Kõik kotletid on praetud!")
